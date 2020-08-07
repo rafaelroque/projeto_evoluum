448a2bed4cb90ba1f597b9675fd9b0a1e8d01a67
Cinthia Bastos <cinthiabastos@MacBook-Air-de-Cinthia-2.local>
2020-08-06 21:18:24 -0300

    Primeiro commit

---
+++ src/main/java/com/evoluum/api/ProjetoEvoluumApplication.java
@@ -0,0 +1,13 @@
+package com.evoluum.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEvoluumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEvoluumApplication.class, args);
+	}
+
+}
