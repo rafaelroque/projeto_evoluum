bb6293fd97f95c22bacce762b07967062f2b48e9
Rafael Roque <cinthiabastos@MacBook-Air-de-Cinthia-2.local>
2020-08-10 10:59:45 -0300

    Exception Handler

--- src/main/java/com/evoluum/api/exception/handler/ListagemExceptionHandler.java
+++ src/main/java/com/evoluum/api/exception/handler/ListagemExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import com.evoluum.api.exception.ListagemException;
 
+
 @ControllerAdvice
 public class ListagemExceptionHandler {
 	
